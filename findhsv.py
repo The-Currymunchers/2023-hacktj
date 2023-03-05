0bb108134dcabd18e04e7573f5a78164f1bcbb3b
salban <salban.nithilaselvan@gmail.com>
2023-03-05 00:19:52 -0500

    im good dog

--- findhsv.py
+++ findhsv.py
@@ -3,4 +3,17 @@ import numpy as np
 from cvzone.ColorModule import ColorFinder
 import cv2 as cv
 
-image = cv.imread()
+hsvVals = {'hmin': 0, 'smin': 122, 'vmin': 174, 'hmax': 43, 'smax': 255, 'vmax': 255}
+
+ballColorFinder = ColorFinder(False)
+
+while True:
+    image = cv.imread("../2023-HACKTJ/ball_image.png")
+    imageColor, mask = ballColorFinder.update(image, hsvVals)
+
+    imageStack = cvzone.stackImages([image, imageColor], 2, 0.5)
+
+    cv.imshow("image", imageStack)
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
